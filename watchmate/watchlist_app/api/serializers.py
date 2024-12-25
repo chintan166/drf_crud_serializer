9aed2f4707177ca805a8a561831731255aa4fea9
chintan166 <chintansuthar61@gmail.com>
2024-12-25 14:53:00 +0530

    gti

---
+++ watchmate/watchlist_app/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from watchlist_app.models import Movie
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    description  = serializers.CharField()
+    active = serializers.BooleanField()
+    
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self,instance,validate_data):
+        instance.name = validate_data.get('name',instance.name)
+        instance.description = validate_data.get('description',instance.description)
+        instance.active = validate_data.get('active',instance.active)
+        instance.save()
+        return instance
+        
